7ee0d09e2fda211057143ad2ea5035e922c4b240
Delianie <delia.niederberger@bluewin.ch>
2025-12-04 12:23:50 +0100

    schwarze Infinity

--- script-startseite.js
+++ script-startseite.js
@@ -1,21 +1,21 @@
 /* ========= DATEN ========= */
 
 const images = [
-    { src: "images-startseite/1.webp", title: "Typografie aus Technik", subtitle: "FONTDESIGN | ILLUSTRATION | TYPOGRAFIE", link: "html-project/03_ilustrarionfont.html" },
+    { src: "images-startseite/neu/ilu.webp", title: "Typografie aus Technik", subtitle: "FONTDESIGN | ILLUSTRATION | TYPOGRAFIE", link: "html-project/03_ilustrarionfont.html" },
 
-    { src: "images-startseite/2.webp", title: "Intensität in der Tiefe", subtitle: "CORPORATE DESIGN | ILLUSTRATION", link: "html-project/06_bildwort.html" },
+    { src: "images-startseite/neu/coop.webp", title: "Intensität in der Tiefe", subtitle: "CORPORATE DESIGN | ILLUSTRATION", link: "html-project/06_bildwort.html" },
 
-    { src: "images-startseite/2.mp4", title: "Aus dem Auge, aus dem Sinn?", subtitle: "POSTERDESIGN", link: "html-project/01_blickwechselplakat.html" },
+    { src: "images-startseite/neu/blick.webp", title: "Aus dem Auge, aus dem Sinn?", subtitle: "POSTERDESIGN", link: "html-project/01_blickwechselplakat.html" },
 
-    { src: "images-startseite/bild4.webp", title: "Kreislauf von Gegenständen", subtitle: "ZEITUNG | FOTOGRAFIE", link: "html-project/02_zeitung.html" },
+    { src: "images-startseite/neu/master.webp", title: "Musik in Plakat", subtitle: "ZEITUNG | FOTOGRAFIE", link: "html-project/02_zeitung.html" },
 
-    { src: "images-startseite/5.webp", title: "Delia Niederberger", subtitle: "SPORT | DESIGN | FAMILIE", link: "html-project/08_ich.html" },
+    { src: "images-startseite/neu/ich.webp", title: "Delia Niederberger", subtitle: "SPORT | DESIGN | FAMILIE", link: "html-project/08_ich.html" },
 
-    { src: "images-startseite/bild6.webp", title: "Versteckte Geräusche von London", subtitle: "EDITORIAL DESIGN", link: "html-project/04_london.html" },
+    { src: "images-startseite/neu/london.webp", title: "Versteckte Geräusche von London", subtitle: "EDITORIAL DESIGN", link: "html-project/04_london.html" },
 
-    { src: "images-startseite/5.mp4", title: "Neuinterpretation der Schweizer Typografie", subtitle: "MOTION DESIGN | TYPOGRAFIE", link: "html-project/05_motiontype.html" },
+    { src: "images-startseite/neu/motion.webp", title: "Neuinterpretation der Schweizer Typografie", subtitle: "MOTION DESIGN | TYPOGRAFIE", link: "html-project/05_motiontype.html" },
 
-    { src: "images-startseite/7.webp", title: "Siebdruck – Farben übereinander", subtitle: "PREPRESS | COLOR MANAGEMENT | POSTERDESIGN", link: "html-project/07_preepress.html" }
+    { src: "images-startseite/neu/prepress.webp", title: "Siebdruck – Farben übereinander", subtitle: "PREPRESS | COLOR MANAGEMENT | POSTERDESIGN", link: "html-project/07_preepress.html" }
 ];
 
 /* ========= ELEMENTE ========= */
